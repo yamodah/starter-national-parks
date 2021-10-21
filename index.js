312ded4210e3124e970c7667be628d8a5c63404a
yamodah <yassinedahlek@gmail.com>
2021-10-21 18:38:28 -0400

    making form submission data stuff

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
+const submitHandler = (event) => {
+  event.preventDefault();
+  console.log("The form was submitted");
+  // Get the name input
+  const parkName = document.querySelector("#name-input").value;
+  const formData = new FormData(event.target);
+  console.log(formData.get("location"));
+  //console.log(parkName);
+};
+const main = () => {
+  // Get the form element
+  const form = document.querySelector("#park-form");
 
+  // Attach the submit handler
+  form.addEventListener("submit", submitHandler);
+};
+window.addEventListener("DOMContentLoaded", main);
+function validateExists(value) {
+  return value && value.trim();
+}
+function validateForm(formData) {
+  const errors = {};
+
+  // Check if name was entered
+  if (!validateExists(formData.get("name"))) {
+    errors.name = "Please enter a name";
+  }
+
+  // Check if rating was entered
+  if (!validateExists(formData.get("rating"))) {
+    errors.rating = "Please enter a rating";
+  }
+
+  // Check if description was entered
+  if (!validateExists(formData.get("description"))) {
+    errors.description = "Please enter short description";
+  }
+
+  // Check if established date was entered
+  if (!validateExists(formData.get("established"))) {
+    errors.established = "Please enter date";
+  }
+
+  // Check if area was entered
+  if (!validateExists(formData.get("area"))) {
+    errors.area = "Please enter the area of the park";
+  }
+
+  // Check if location date was entered
+  if (!validateExists(formData.get("location"))) {
+    errors.location = "Please enter the location of the park";
+  }
+
+  return errors;
+}
