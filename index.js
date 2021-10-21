42e1bb71999cc55f4965a3a0ed10a55349478637
yamodah <yassinedahlek@gmail.com>
2021-10-21 15:50:30 -0400

    functioning sort by name link

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ nameSorter.addEventListener("click", (event) => {
 nameSorter.addEventListener("click", (event) => {
     event.preventDefault();
   
-    // 1.  Get the main element
+    // 1. Get the main element
     const main = document.querySelector("main");
   
     // 2. Get the list of parks
@@ -93,4 +93,25 @@ nameSorter.addEventListener("click", (event) => {
   
     // 3. Empty the main element
     main.innerHTML = "";
+  
+    // 4. Create an array
+    const parksArray = Array.from(parksList);
+  
+    // 5. Sort the array
+    parksArray.sort((parkA, parkB) => {
+      const parkAName = parkA.querySelector("h2").innerText;
+      const parkBName = parkB.querySelector("h2").innerText;
+      if (parkAName < parkBName) {
+        return -1;
+      } else if (parkAName > parkBName) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  
+    // 6. Insert each park into the DOM
+    parksArray.forEach((park) => {
+      main.appendChild(park);
+    });
   });
